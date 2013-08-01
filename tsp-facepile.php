a5a1390687a0ea7d0e5106337d8de8faebe83f62
Sharron Denice <sharron@me.com>
2013-07-31 20:27:55 -0500

    * Stable Build * Instead of checking for the existence of a class check to see if the plugin exists in the directory, activation of Easy Dev is not required if other plugins need to use it Removed the admin menu template, it will be supplied by Easy Dev Ticket #FCP-2

--- tsp-facepile.php
+++ tsp-facepile.php
@@ -21,7 +21,9 @@ define('TSPFCP_PLUGIN_NAME', 				'tsp-facepile');
 define('TSPFCP_PLUGIN_TITLE', 				'TSP Facepile');
 define('TSPFCP_PLUGIN_REQ_VERSION', 		"3.5.1");
 
-if (!class_exists('TSP_Easy_Dev'))
+// If either of the plugins exist then inlude the register script to register all the classes
+// else deactivaet this plugin and do not allow installation
+if ( !file_exists ( WP_PLUGIN_DIR . "/tsp-easy-dev/TSP_Easy_Dev.register.php" ) && !file_exists( WP_PLUGIN_DIR . "/tsp-easy-dev-pro/TSP_Easy_Dev.register.php" ) )
 {
 	add_action( 'admin_notices', function (){
 		
@@ -37,6 +39,17 @@ if (!class_exists('TSP_Easy_Dev'))
 	
 	return;
 }//endif
+else
+{
+    if (file_exists( WP_PLUGIN_DIR . "/tsp-easy-dev-pro/TSP_Easy_Dev.register.php" ))
+    {
+    	include_once WP_PLUGIN_DIR . "/tsp-easy-dev-pro/TSP_Easy_Dev.register.php";
+    }//end if
+    elseif (file_exists( WP_PLUGIN_DIR . "/tsp-easy-dev/TSP_Easy_Dev.register.php" ))
+    {
+    	include_once WP_PLUGIN_DIR . "/tsp-easy-dev/TSP_Easy_Dev.register.php";
+    }//end else
+}//end else
 
 global $easy_dev_settings;
 
